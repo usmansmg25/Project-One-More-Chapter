2f9a1b059865d8703568a6f7bf0071c40dcbfeba
Mohammed Usman <mohammed.usman@globallogic.com>
2022-12-06 21:10:06 +0530

    initial commit

---
+++ cart-service/src/test/java/com/stackroute/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
